31a68b883057a4c12828e4cfb971644e3199a1ed
Jose Felix Coronado <jfcoronado@gmail.com>
2022-10-18 19:30:20 -0500

    change name of config

--- Cloud Native Applications/Lab 07 - ECS/config-example.py
+++
@@ -1,6 +0,0 @@
-ecs_test_drive = {
-    'ecs_endpoint_url' : 'http://ecs1.nolalab.local:9020',
-    'ecs_access_key_id' : 'object_admin1',
-    'ecs_secret_key' : 'ChangeMeChangeMeChangeMeChangeMeChangeMe',
-    'ecs_bucket_name' : 'photo-album'
-}
